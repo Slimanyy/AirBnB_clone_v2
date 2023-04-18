fcf5ba3271f2d360722ddd9c22f17f4f2c559442
Abdulkareem Abdulquddus Olayinka <aabdulqudduss@gmail.com>
2023-04-18 22:59:13 +0100

    updating py files for storage purposes

--- models/user.py
+++ models/user.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
-"""This module creates a User class"""
-from models.base_model import BaseModel
+"""This is the user class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """Class for managing user objects"""
+class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = 'users'
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place', backref='user',
+                          cascade='all, delete-orphan')
+    reviews = relationship('Review', backref='user',
+                           cascade='all, delete-orphan')
