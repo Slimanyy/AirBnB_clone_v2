49648d74ff463b2b55d666ae2cf8876354d72ced
Slimanyy <aabdulqudduss@gmail.com>
2023-05-21 13:45:48 +0000

    8-sities updated

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,17 +9,17 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """display the states and cities listed in alphabetical order"""
-    states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=states)
-
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display the states and cities listed in alphabetical order"""
+    states = sorted(storage.all(State).values(), key=lambda s: s.name)
+    return render_template('8-cities_by_states.html', states=states)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
