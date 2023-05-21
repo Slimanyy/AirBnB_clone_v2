efa567e2ec6a049913d6f25d67460717c78732ce
Abdulkareem Abdulquddus Olayinka <aabdulqudduss@gmail.com>
2023-05-21 10:53:44 +0100

    models cga

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
-"""Initializes the package"""
+"""This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-storage = FileStorage()
+from os import getenv
+from models.engine.db_storage import DBStorage
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from os import getenv
-from sqlalchemy import String, DateTime, Column, ForeignKey
-from sqlalchemy.orm import relationship
-import models
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from models.city import City
+import models
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
+    """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
-    else:
-        @property
-        def cities(self):
-            """Getter attribute in case of file storage"""
-            return [city for city in models.storage.all(City).values()
-                    if city.state_id == self.id]
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='delete, all, delete-orphan',
+                          backref='state')
+
+    @property
+    def cities(self):
+        """FileStorage relationship between State and City"""
+        city_list = []
+        for city in models.storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
